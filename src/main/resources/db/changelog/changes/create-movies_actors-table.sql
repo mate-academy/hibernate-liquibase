1dfb0da44174dad7ce391f5aa7abeec46d6eb082
Yura Khilko <yurakhilko@gmail.com>
2023-08-04 11:20:31 +0300

    solution №2

--- src/main/resources/db/changelog/changes/create-movies_actors-table.sql
+++ src/main/resources/db/changelog/changes/create-movies_actors-table.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 --changeset yuriykhilko:create-movies_actors-table splitStatements:true endDelimiter:;
-create table movies_actors (movie_id bigint not null, actor_id bigint not null) engine=MyISAM;
+create table movies_actors (actor_id bigint not null, movie_id bigint not null);
+--create table movies_actors (movie_id bigint not null, actor_id bigint not null) engine=MyISAM;
 
 --rollback DROP TABLE movies_actors;
--- src/main/resources/db/changelog/changes/movies_actorsFKtwo.sql
+++ src/main/resources/db/changelog/changes/movies_actorsFKtwo.sql
@@ -1,1 +1,2 @@
- alter table movies_actors add constraint dawd foreign key (movie_id) references movies (id);
+-- alter table movies_actors add constraint dawd foreign key (movie_id) references movies (id);
+alter table movies_actors add constraint FKo40lc9yirox8um5poaxhw6ak0 foreign key (movie_id) references movies;
