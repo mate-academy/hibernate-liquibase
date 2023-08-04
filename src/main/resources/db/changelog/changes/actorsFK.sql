d15b3ed358dd81543b9561f72f43ca461e769ce6
Yura Khilko <yurakhilko@gmail.com>
2023-08-04 11:52:34 +0300

    solution №4

--- src/main/resources/db/changelog/changes/actorsFK.sql
+++ src/main/resources/db/changelog/changes/actorsFK.sql
@@ -1,2 +1,1 @@
- --alter table actors add constraint adwD foreign key (country_id) references countries (id);
  alter table actors add constraint FK2yo5yhg5xhkxjsdqcmlk8qali foreign key (country_id) references countries;
--- src/main/resources/db/changelog/changes/movies_actorsFKone.sql
+++ src/main/resources/db/changelog/changes/movies_actorsFKone.sql
@@ -1,2 +1,1 @@
- --alter table movies_actors add constraint adwad foreign key (actor_id) references actors (id);
  alter table movies_actors add constraint FKlexe564x1fi4ssje5wcuii3v2 foreign key (actor_id) references actors;
