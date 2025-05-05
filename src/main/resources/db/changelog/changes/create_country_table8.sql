5fc8b1e9e35651bbc23a4fa2ed9adf13c585e60d
Teslenko Daniil <burgalhashem@gmail.com>
2025-05-05 14:14:58 +0300

    setup liquibase migration

--- src/main/resources/db/changelog/changes/create_country_table8.sql
+++
@@ -1,4 +0,0 @@
-create table countries (
-id bigint not null auto_increment,
-name varchar(255),
-primary key (id));
--- src/main/resources/db/changelog/changes/create_movie_table8.sql
+++
@@ -1,5 +0,0 @@
-create table movies (
-id bigint not null auto_increment,
-title varchar(255),
-primary key (id),
-actors bigint);
